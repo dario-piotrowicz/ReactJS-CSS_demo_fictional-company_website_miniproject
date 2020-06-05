758e380651fe6421f6987c772fb68c198c9a6053
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-06-05 20:41:06 +0200

    made FormComponent sub-component in ContactSection

--- src/components/contact-section/contact-section.component.jsx
+++ src/components/contact-section/contact-section.component.jsx
@@ -1,6 +1,28 @@
 import React from 'react';
 import './contact-section.styles.scss';
 import contact from '../../assets/images/home/contact.jpg';
+import PropTypes from 'prop-types';
+
+const FormControl = ({ name, type }) => {
+  const nameCaptalized = name.charAt(0).toUpperCase() + name.slice(1);
+  const inputId = `contact-section-form-control-${name}`;
+  const inputType = type ? type : 'text';
+  return (
+    <div className="form-control">
+      <label htmlFor={inputId}>{nameCaptalized}</label>
+      <input
+        type={inputType}
+        id={inputId}
+        name={`contact-form-control-${name}`}
+        placeholder={`Enter ${name}`}
+      />
+    </div>
+  );
+};
+FormControl.propTypes = {
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string,
+};
 
 const ContactSection = () => {
   return (
@@ -11,28 +33,9 @@ const ContactSection = () => {
       <div className="content">
         <h2>Request something!</h2>
         <form action="">
-          <div className="form-control">
-            <label htmlFor="name">Name</label>
-            <input type="text" id="name" name="name" placeholder="Enter name" />
-          </div>
-          <div className="form-control">
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Enter email"
-            />
-          </div>
-          <div className="form-control">
-            <label htmlFor="phone">Phone</label>
-            <input
-              type="text"
-              id="phone"
-              name="phone"
-              placeholder="Enter phone"
-            />
-          </div>
+          <FormControl name="name" />
+          <FormControl name="email" type="email" />
+          <FormControl name="phone" />
           <input type="submit" value="Send" id="submit" className="btn" />
         </form>
       </div>
