26e1415ed84c3ec3a30129a791c3163eb1ad0faa
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-06-06 18:28:52 +0200

    implemented header with empty background where appropriate

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './header.styles.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMoneyBillWave } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 const Header = () => {
+  const location = useLocation();
+  const isHomeOrBlog =
+    location.pathname === '/' || location.pathname.startsWith('/blog');
+
+  const [transparentBg, setTransparentBg] = useState(true);
+  useEffect(() => {
+    if (!isHomeOrBlog) return;
+
+    const updateBackgroundOnScroll = () => {
+      const yOffset = window.pageYOffset;
+      if (yOffset > 250) {
+        setTransparentBg(false);
+      } else {
+        setTransparentBg(true);
+      }
+    };
+    window.addEventListener('scroll', updateBackgroundOnScroll);
+    return () => window.removeEventListener('scroll', updateBackgroundOnScroll);
+  }, [isHomeOrBlog, window]);
+
   return (
-    <header id="main-header">
+    <header
+      id="main-header"
+      className={`${isHomeOrBlog && transparentBg ? 'transparent-bg' : ''}`}
+    >
       <h1>
         <span className="text-primary">
           <span className="fa-icon">
